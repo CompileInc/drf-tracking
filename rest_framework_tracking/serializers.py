af7b6653dda74e1158b536b9b6412a6255f465b3
Cyriac Thomas <cyriac@compile.com>
2016-05-26 14:17:22 +0530

    usage report is now per urlpattern

--- rest_framework_tracking/serializers.py
+++ rest_framework_tracking/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
 
 class APIRequestLogSerializer(serializers.Serializer):
-    current = serializers.IntegerField()
-    previous = serializers.IntegerField()
+    current = serializers.DictField()
+    previous = serializers.DictField()
--- rest_framework_tracking/views.py
+++ rest_framework_tracking/views.py
@@ -1,15 +1,20 @@
 from django.conf import settings
+from django.contrib.admindocs.views import simplify_regex
 from django.contrib.sites.models import Site
+from django.core.urlresolvers import RegexURLResolver, RegexURLPattern
 from django.core.validators import EMPTY_VALUES
+from django.utils.module_loading import import_string
 
-from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import APIRequestLog
 from .serializers import APIRequestLogSerializer
 
 from datetime import date
 from datetime import timedelta
+from importlib import import_module
+import re
 
 
 FILTER_CURRENT_HOST = getattr(settings, 'DRF_TRACKING_USAGE_CURRENT_SITE', True)
@@ -49,12 +54,62 @@ class APIRequestList(APIView):
         end = date(today.year, start.month+1, 1) - timedelta(days=1)
         return [start, end]
 
+    def get_urlconf(self):
+        urlconf = settings.ROOT_URLCONF
+        if hasattr(self.request, 'urlconf'):
+            urlconf = self.request.urlconf
+
+        try:
+            root_urlconf = import_string(urlconf)
+        except ImportError:
+            root_urlconf = import_module(urlconf)
+
+        return root_urlconf
+
+    def _is_drf_view(self, pattern):
+        return hasattr(pattern.callback, 'cls') and issubclass(pattern.callback.cls, APIView)
+
+    def _is_format_endpoint(self, pattern):
+        return '?P<format>' in pattern._regex
+
+    def get_all_regex_patterns(self, urlpatterns, parent_pattern=None):
+        for pattern in urlpatterns:
+            if isinstance(pattern, RegexURLResolver):
+                parent_pattern = None if pattern._regex == "^" else pattern
+                self.get_all_regex_patterns(urlpatterns=pattern.url_patterns, parent_pattern=parent_pattern)
+            elif isinstance(pattern, RegexURLPattern) and self._is_drf_view(pattern) and not self._is_format_endpoint(pattern):
+                self.url_patterns.append(pattern.regex)
+
+    def get_urlconf_patterns(self):
+        root_urlconf = self.get_urlconf()
+        self.url_patterns = []
+
+        if hasattr(root_urlconf, 'urls'):
+            self.get_all_regex_patterns(root_urlconf.urls.urlpatterns)
+        else:
+            self.get_all_regex_patterns(root_urlconf.urlpatterns)
+
+        return self.url_patterns
+
+    def get_path_counts(self, patterns, qs):
+        counts = {}
+        for regex in patterns:
+            counts[regex.pattern] = qs.filter(path__regex=simplify_regex(regex.pattern)).count()
+        return counts
 
     def get(self, request, *args, **kwargs):
+        patterns = self.get_urlconf_patterns()
         qs = self.get_queryset()
+
         today = date.today()
+        current_qs = qs.filter(requested_at__range=self.get_window(today, 0))
+        previous_qs = qs.filter(requested_at__range=self.get_window(today, -1))
+
+        current = self.get_path_counts(patterns, current_qs)
+        previous = self.get_path_counts(patterns, previous_qs)
+
         data = {
-                'current': qs.filter(requested_at__range=self.get_window(today, 0)).count(),
-                'previous': qs.filter(requested_at__range=self.get_window(today, -1)).count()
+                'current': current,
+                'previous': previous,
                 }
         return Response(APIRequestLogSerializer(data).data)
